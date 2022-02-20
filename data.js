bff14a17a8136c466fdb546edb9f4aed4dd35565
neomq <neominqi@hotmail.com>
2022-02-20 17:34:31 +0000

    added search function and some styling

--- data.js
+++ data.js
@@ -2,13 +2,13 @@ const API_BASE_URL="https://api.foursquare.com/v3";
 const API_KEY="fsq39KAcQFbTaS1UcZfkT7ZQAkGv7nIP4Wtkhziu4dcLibg=";
 
 // search function
-async function search(query, location){
+async function search(keyword, location){
     
     let near = 'singapore' + ',' + location; // string seperated by comma
 
     let response = await axios.get(API_BASE_URL + '/places/search', {
         params: {
-            'query': query,
+            'query': keyword,
             'categories': '11128',
             'near': near,
             'v': '02202022'
--- script.js
+++ script.js
@@ -6,37 +6,49 @@ async function main(){
         let mapObject = initMap();
 
         window.addEventListener('DOMContentLoaded', function(){
-            // add event listeners here
-            
+            // add event listeners here...
+
             // Search bar dropdown toggle
             document.querySelector('#itemtext').addEventListener('click', function(){
-                //alert('clicked!')
+                // alert('clicked!')
                 let text = document.querySelector('#buttontext').innerText;
                 document.querySelector('#buttontext').innerText = document.querySelector('#itemtext').innerText;
                 document.querySelector('#itemtext').innerText = text;
             })
-            
+            // Search function
+            document.querySelector('#search-btn').addEventListener('click', async function(){
+                let keyword = "";
+                let location = "";
+
+                if (document.querySelector('#buttontext').innerText == 'Keyword'){
+                    keyword = document.querySelector("#search-input").value;
+                } else if (document.querySelector('#buttontext').innerText == 'Location'){
+                    location = document.querySelector("#search-input").value;
+                }
+                let response = await search(keyword, location);
+                console.log(response)
+            })
         })
+
+        // Map Setup
+        function initMap() {
+            let singapore = [1.29, 103.85];
+            let mapObject = L.map('sgmap').setView(singapore, 13);
+
+            // Tile layers boilerplate
+            L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+                maxZoom: 18,
+                id: 'mapbox/streets-v11',
+                tileSize: 512,
+                zoomOffset: -1,
+                accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw' //demo access token
+            }).addTo(mapObject);
+
+            return mapObject;
+        }
+
     }
-
-    // Map Setup
-    function initMap() {
-        let singapore = [1.29, 103.85];
-        let mapObject = L.map('sgmap').setView(singapore, 13);
-
-        // Tile layers boilerplate
-        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-            maxZoom: 18,
-            id: 'mapbox/streets-v11',
-            tileSize: 512,
-            zoomOffset: -1,
-            accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw' //demo access token
-        }).addTo(mapObject);
-
-        return mapObject;
-    }
-
     init();
 }
 main();
