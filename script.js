a0322b41b85a017bd2bd5ebea2933d68e46cf142
neomq <neo.minqi@trentglobal.edu.sg>
2022-03-06 16:05:28 +0000

    fixed search bug

--- script.js
+++ script.js
@@ -48,8 +48,11 @@ const workspaceMarker = L.icon({
     popupAnchor: [4, 0]
 });
 
+// set search results div as global variable
+let searchResultElement = document.querySelector('#search-results');
+
 // Execute search
-async function startSearch() {
+function startSearch() {
 
     // update leaflet map
     mapObject.invalidateSize();
@@ -88,8 +91,7 @@ async function startSearch() {
         }
     };
 
-    // set search results panel div as global variable
-    let searchResultElement = document.querySelector('#search-results');
+    // console.log("checking:", keyword, location); // checking only
 
     // Validation of search query
     let specialChars = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
@@ -115,209 +117,209 @@ async function startSearch() {
         displayErrorText.className = 'card-body';
         
         if (locationNotValid) {
-            displayErrorText.innerHTML = `Please enter a valid location`;
+            displayErrorText.innerHTML = `No search results. Please enter a valid location`;
         }
         if (keywordNotValid) {
-            displayErrorText.innerHTML = `Please enter a valid keyword`;
+            displayErrorText.innerHTML = `No search results. Please enter a valid keyword`;
         }
         if (locationNotValid && keywordNotValid) {
-            displayErrorText.innerHTML = `Please enter a valid location and keyword`;
+            displayErrorText.innerHTML = `No search results. Please enter a valid location and keyword`;
         }
 
         searchResultElement.appendChild(displayError);
         displayError.appendChild(displayErrorText);
 
     } else {
+        // remove display of results summary
         document.querySelector('#results-header').style.display = null;
+        
         // pass search query to foursquare api and display results
-        getSearchResults ();
+        getSearchResults (keyword, location);
+    }
+} // end of startSearch
+
+// Get search results
+async function getSearchResults (keyword, location){
+    let near = `${location}, Singapore`;
+    let response1 = await search(keyword, near);
+    // console.log("search for", keyword, "near ", near)
+    // console.log(response1);
+
+    // display search summary and number of search results
+    let resultsNumber = response1.results.length;
+    if (keyword && location) {
+        document.querySelector('#results-title').innerHTML = `Search results for ${keyword} near ${near}`;
+        document.querySelector('#results-number').innerHTML = `${resultsNumber} results`;
+    } else if (keyword && !location) {
+        document.querySelector('#results-title').innerHTML = `Search results for ${keyword} in Singapore`;
+        document.querySelector('#results-number').innerHTML = `${resultsNumber} results`;
+    } else if (location && !keyword) {
+        document.querySelector('#results-title').innerHTML = `Search results near ${near}`;
+        document.querySelector('#results-number').innerHTML = `${resultsNumber} results`;
+    } else {
+        document.querySelector('#results-title').innerHTML = `Suggested Co-working Spaces in Singapore`;
+        document.querySelector('#results-number').innerHTML = `${resultsNumber} suggestions`;
     }
 
-    async function getSearchResults (){
-        let near = `${location}, Singapore`
+    // display results filter
+    let searchFilter = document.querySelector('#results-filter');
+    searchFilter.style.display = "block";
 
-        let response1 = await search(keyword, near);
-        console.log("search: ", keyword, "near ", near)
-        // console.log(response1);
+    // map layers toggle to appear
+    document.querySelector("#layer-container").style.display = "block";
 
-        // display search summary and number of search results
-        let resultsNumber = response1.results.length;
-        if (keyword && location) {
-            document.querySelector('#results-title').innerHTML = `Search results for ${keyword} near ${near}`;
-            document.querySelector('#results-number').innerHTML = `${resultsNumber} results`;
-        } else if (keyword && !location) {
-            document.querySelector('#results-title').innerHTML = `Search results for ${keyword} in Singapore`;
-            document.querySelector('#results-number').innerHTML = `${resultsNumber} results`;
-        } else if (location && !keyword) {
-            document.querySelector('#results-title').innerHTML = `Search results near ${near}`;
-            document.querySelector('#results-number').innerHTML = `${resultsNumber} results`;
+    // map markers
+    for (let eachResult of response1.results) {
+        let lat = eachResult.geocodes.main.latitude;
+        let lng = eachResult.geocodes.main.longitude;
+        let coordinates = [lat, lng];
+
+        // create marker
+        let searchMarker = L.marker(coordinates, { icon: workspaceMarker });
+
+        // get address from results
+        let address = eachResult.location.formatted_address;
+
+        // get fsq_id from results
+        let fsqid = eachResult.fsq_id;
+
+        // get photos
+        let response2 = await searchPhotos(fsqid);
+        // console.log(response2);
+        let photoUrl = "";
+        if (response2[0]) {
+            let prefix = response2[0].prefix;
+            let suffix = response2[0].suffix;
+            photoUrl = prefix + '180x135' + suffix;
+        }
+
+        // get place information
+        let response3 = await searchPlaceDetails(fsqid);
+        // console.log(response3);
+
+        // get website
+        let website = "";
+        if (response3.website) {
+            website = response3.website;
+        }
+        // get opening hours
+        let openNow = "";
+        if (response3.hours.open_now === true) {
+            openNow = "Open";
         } else {
-            document.querySelector('#results-title').innerHTML = `Suggested Co-working Spaces in Singapore`;
-            document.querySelector('#results-number').innerHTML = `${resultsNumber} suggestions`;
+            openNow = "Closed";
+        }
+        let openingHours = "";
+        if (response3.hours.display) {
+            openingHours = response3.hours.display;
+        } else {
+            openingHours = "No Opening Hours Available";
         }
 
-        // display results filter
-        let searchFilter = document.querySelector('#results-filter');
-        searchFilter.style.display = "block";
-
-        // map layers toggle to appear
-        document.querySelector("#layer-container").style.display = "block";
-
-        // map markers
-        for (let eachResult of response1.results) {
-            let lat = eachResult.geocodes.main.latitude;
-            let lng = eachResult.geocodes.main.longitude;
-            let coordinates = [lat, lng];
-
-            // create marker
-            let searchMarker = L.marker(coordinates, { icon: workspaceMarker });
-
-            // get address from results
-            let address = eachResult.location.formatted_address;
-
-            // get fsq_id from results
-            let fsqid = eachResult.fsq_id;
-
-            // get photos
-            let response2 = await searchPhotos(fsqid);
-            // console.log(response2);
-            let photoUrl = "";
-            if (response2[0]) {
-                let prefix = response2[0].prefix;
-                let suffix = response2[0].suffix;
-                photoUrl = prefix + '180x135' + suffix;
-            }
-
-            // get place information
-            let response3 = await searchPlaceDetails(fsqid);
-            // console.log(response3);
-
-            // get website
-            let website = "";
-            if (response3.website) {
-                website = response3.website;
-            }
-            // get opening hours
-            let openNow = "";
-            if (response3.hours.open_now === true) {
-                openNow = "Open";
-            } else {
-                openNow = "Closed";
-            }
-            let openingHours = "";
-            if (response3.hours.display) {
-                openingHours = response3.hours.display;
-            } else {
-                openingHours = "No Opening Hours Available";
-            }
-
-            // get nearby food and dining info 
-            let ll = `${lat},${lng}`;
-            let response4 = await searchNearFood(ll);
-            //console.log(response4);
-            let nearFood = "";
-            //let foodArray = [];
-            let searchNearbyFood;
-            for (let eachFoodResult of response4.results) {
-                nearFood = eachFoodResult.name;
-                nearFoodAddress = eachFoodResult.location.formatted_address;
-                nearFoodDistance = eachFoodResult.distance;
-                searchNearbyFood = L.marker([eachFoodResult.geocodes.main.latitude, eachFoodResult.geocodes.main.longitude]);
-                searchNearbyFood.bindPopup(`<div>
-                                        <p>${nearFood}</p>
-                                        <p>${nearFoodAddress}</p>
-                                        <p>${nearFoodDistance}m away</p>
-                                    </div>`);
-                searchNearbyFood.addTo(nearbyFoodLayer);
-                //foodArray.push(nearFood);
-            }
-
-            // create popup content
-            let popupContent = document.createElement('div');
-            popupContent.className = 'py-1';
-
-            let popupTitle = document.createElement('h6');
-            popupTitle.className = 'mt-2';
-            popupTitle.innerHTML = eachResult.name;
-
-            let popupPhoto = `<img src="${photoUrl}" alt="">`;
-
-            let popupAddress = document.createElement('p');
-            popupAddress.className = 'fs-6 text-muted mt-1 mb-2';
-            popupAddress.innerHTML = address;
-
-            // let popupLatLng = document.createElement('p'); // for checking only
-            // popupLatLng.innerHTML = `${lat},${lng}`; // for checking only
-
-            popupContent.appendChild(popupTitle);
-            if (photoUrl !== "") {
-                popupContent.innerHTML += popupPhoto;
-            };
-            popupContent.appendChild(popupAddress);
-            // popupContent.appendChild(popupLatLng);  // for checking only
-
-            // add popup content to marker
-            searchMarker.bindPopup(popupContent, { maxWidth: 180 });
-
-            // add marker to search layer
-            searchMarker.addTo(searchResultLayer);
-
-            // append search results to searchResultElement
-            // let searchResultElement = document.querySelector('#search-results'); // set as global variable
-
-            let resultElement = document.createElement('div');
-            resultElement.className = 'search-result card bg-transparent mb-3 p-2';
-
-            let resultElementCard = document.createElement('div');
-            resultElementCard.className = 'card-body';
-
-            let resultTitle = document.createElement('h6');
-            resultTitle.className = 'card-title pb-2';
-
-            let resultHours = document.createElement('p');
-            if (openNow === "Open") {
-                resultHours.className = 'card-category open my-2';
-            } else {
-                resultHours.className = 'card-category close my-2';
-            }
-
-            let resultLocation = document.createElement('p');
-            resultLocation.className = 'card-text mb-2';
-
-            // let resultWeb = document.createElement('div');
-            // resultWeb.className = 'mt-3';
-
-            resultTitle.innerHTML = eachResult.name;
-            resultHours.innerHTML = `<i class="fa-regular fa-clock"></i>&nbsp;&nbsp;${openNow} • ${openingHours}`;
-            resultLocation.innerHTML += address;
-            // resultWeb.innerHTML = `<a class="btn btn-outline-secondary btn-sm" href="${website}" role="button">Visit Website</a>`;
-
-            searchResultElement.appendChild(resultElement);
-            resultElement.appendChild(resultElementCard);
-            resultElementCard.appendChild(resultTitle);
-            resultElementCard.appendChild(resultHours);
-            resultElementCard.appendChild(resultLocation);
-
-            if (website !== "") {
-                let resultWeb = document.createElement('div');
-                resultWeb.className = 'mt-3';
-                resultWeb.innerHTML = `<a class="btn btn-outline-secondary btn-sm" href="${website}" target="_blank" role="button">Visit Website</a>`;
-                resultElementCard.appendChild(resultWeb);
-            }
-
-            // Event listener to resultElement
-            resultElement.addEventListener('click', function () {
-                // zoom to the location on map
-                mapObject.flyTo(coordinates, 18);
-                searchMarker.openPopup();
-            })
+        // get nearby food and dining info 
+        let ll = `${lat},${lng}`;
+        let response4 = await searchNearFood(ll);
+        //console.log(response4);
+        let nearFood = "";
+        //let foodArray = [];
+        let searchNearbyFood;
+        for (let eachFoodResult of response4.results) {
+            nearFood = eachFoodResult.name;
+            nearFoodAddress = eachFoodResult.location.formatted_address;
+            nearFoodDistance = eachFoodResult.distance;
+            searchNearbyFood = L.marker([eachFoodResult.geocodes.main.latitude, eachFoodResult.geocodes.main.longitude]);
+            searchNearbyFood.bindPopup(`<div>
+                                    <p>${nearFood}</p>
+                                    <p>${nearFoodAddress}</p>
+                                    <p>${nearFoodDistance}m away</p>
+                                </div>`);
+            searchNearbyFood.addTo(nearbyFoodLayer);
+            //foodArray.push(nearFood);
         }
 
-        searchResultLayer.addTo(mapObject);
+        // create popup content
+        let popupContent = document.createElement('div');
+        popupContent.className = 'py-1';
 
-    };
+        let popupTitle = document.createElement('h6');
+        popupTitle.className = 'popup-title mt-2';
+        popupTitle.innerHTML = eachResult.name;
 
-} // end of startSearch
+        let popupPhoto = `<img src="${photoUrl}" alt="">`;
+
+        let popupAddress = document.createElement('p');
+        popupAddress.className = 'popup-address mt-3 mb-2';
+        popupAddress.innerHTML = address;
+
+        // let popupLatLng = document.createElement('p'); // for checking only
+        // popupLatLng.innerHTML = `${lat},${lng}`; // for checking only
+
+        popupContent.appendChild(popupTitle);
+        if (photoUrl !== "") {
+            popupContent.innerHTML += popupPhoto;
+        };
+        popupContent.appendChild(popupAddress);
+        // popupContent.appendChild(popupLatLng);  // for checking only
+
+        // add popup content to marker
+        searchMarker.bindPopup(popupContent, { maxWidth: 180 });
+
+        // add marker to search layer
+        searchMarker.addTo(searchResultLayer);
+
+        // append search results to searchResultElement
+        // let searchResultElement = document.querySelector('#search-results'); // set as global variable
+
+        let resultElement = document.createElement('div');
+        resultElement.className = 'search-result card bg-transparent mb-3 p-2';
+
+        let resultElementCard = document.createElement('div');
+        resultElementCard.className = 'card-body';
+
+        let resultTitle = document.createElement('h6');
+        resultTitle.className = 'card-title pb-2';
+
+        let resultHours = document.createElement('p');
+        if (openNow === "Open") {
+            resultHours.className = 'card-category open my-2';
+        } else {
+            resultHours.className = 'card-category close my-2';
+        }
+
+        let resultLocation = document.createElement('p');
+        resultLocation.className = 'card-text mb-2';
+
+        // let resultWeb = document.createElement('div');
+        // resultWeb.className = 'mt-3';
+
+        resultTitle.innerHTML = eachResult.name;
+        resultHours.innerHTML = `<i class="fa-regular fa-clock"></i>&nbsp;&nbsp;${openNow} • ${openingHours}`;
+        resultLocation.innerHTML += address;
+        // resultWeb.innerHTML = `<a class="btn btn-outline-secondary btn-sm" href="${website}" role="button">Visit Website</a>`;
+
+        searchResultElement.appendChild(resultElement);
+        resultElement.appendChild(resultElementCard);
+        resultElementCard.appendChild(resultTitle);
+        resultElementCard.appendChild(resultHours);
+        resultElementCard.appendChild(resultLocation);
+
+        if (website !== "") {
+            let resultWeb = document.createElement('div');
+            resultWeb.className = 'mt-3';
+            resultWeb.innerHTML = `<a class="btn btn-outline-secondary btn-sm" href="${website}" target="_blank" role="button">Visit Website</a>`;
+            resultElementCard.appendChild(resultWeb);
+        }
+
+        // Event listener to resultElement
+        resultElement.addEventListener('click', function () {
+            // zoom to the location on map
+            mapObject.flyTo(coordinates, 18);
+            searchMarker.openPopup();
+        })
+    }
+
+    searchResultLayer.addTo(mapObject);
+};
 
 // Main function
 async function main(){
@@ -469,7 +471,7 @@ async function main(){
             let searchButtons = document.querySelectorAll('#search-btn');
             for (let i of searchButtons) {
                 i.addEventListener('click', async function() {
-
+                    
                     // auto-click to collapse search bar when user clicks on search on md/lg screen
                     let mediaQueryMax991 = window.matchMedia('(max-width: 991px)');
                     // if the max-width: 991px is true
@@ -478,7 +480,7 @@ async function main(){
                         document.querySelector(".navbar-toggler").click();
                     }
 
-                    // start search function
+                    // start search
                     startSearch();
 
                 }); // end of search button click function
@@ -489,6 +491,7 @@ async function main(){
     init();
 }
 main();
+
 
 // SPA
 let page1 = document.querySelector('#page-1');
@@ -564,7 +567,7 @@ document.querySelector('#search-btn').addEventListener('click', function(){
 
     } else {
         toSearchPage();
-        startSearch();
+        // startSearch();
     }
 
 });
